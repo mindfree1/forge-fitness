be59bed3e8564a2c62ad2e04238a854b6c0f6322
Trent <trentd_01@yahoo.com.au>
2026-08-30 19:28:06 +1000

    chore: consolidate Expo config

--- app.config.js
+++ app.config.js
@@ -1,21 +1,45 @@
-const staticConfig = require('./app.json');
 const { ensureForgeIcons } = require('./scripts/generate-forge-icons');
 
 ensureForgeIcons(__dirname);
 
 module.exports = {
   expo: {
-    ...staticConfig.expo,
+    name: 'Forge',
+    slug: 'forge',
+    version: '0.1.0',
+    orientation: 'portrait',
+    scheme: 'forge',
+    userInterfaceStyle: 'dark',
+
     icon: './assets/forge-icon.png',
+
     android: {
-      ...staticConfig.expo.android,
+      package: 'com.mindfree1.forge',
       icon: './assets/forge-icon.png',
       adaptiveIcon: {
-        ...staticConfig.expo.android.adaptiveIcon,
         foregroundImage: './assets/forge-adaptive-icon.png',
         monochromeImage: './assets/forge-adaptive-icon.png',
         backgroundColor: '#10110F',
       },
     },
+
+    plugins: [
+      'expo-router',
+      'expo-sqlite',
+      'expo-font',
+    ],
+
+    experiments: {
+      typedRoutes: true,
+    },
+
+    extra: {
+      router: {},
+      eas: {
+        projectId: '6b6d576a-96c0-4789-88a8-e767485d9a64',
+      },
+    },
+
+    owner: 'mindfree1',
   },
 };
